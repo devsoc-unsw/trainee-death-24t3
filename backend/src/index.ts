646f5487ae6de16848d47e516678abe8515db563
Ben <bengodw@gmail.com>
2024-10-23 13:44:21 +1100

    initialised backend

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const EXPRESS_PORT = 3000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('cotangles backend says hello');
+});
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(
+    `🤝📆 cotangles backend listening on port ${EXPRESS_PORT} 📆🤝`
+  );
+});
