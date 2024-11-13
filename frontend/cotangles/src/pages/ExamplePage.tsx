260c9a5759b96b66a703ddd663f65d13ac5bc385
evaluate09 <eve.miles09@gmail.com>
2024-11-13 12:15:18 +1100

    Installed lucide icon

--- frontend/cotangles/src/pages/ExamplePage.tsx
+++ frontend/cotangles/src/pages/ExamplePage.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+// import { Activity } from "lucide-react";
+
 import "./App.css";
 
 function App() {
