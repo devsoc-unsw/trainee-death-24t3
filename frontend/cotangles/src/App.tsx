c7cab896c2500711fca74aaf1c50cc2fad02bb75
AndyAtmadja03S <atmandy345@gmail.com>
2024-11-13 13:34:47 +1100

    fixed

--- frontend/cotangles/src/App.tsx
+++ frontend/cotangles/src/App.tsx
@@ -5,19 +5,22 @@ import getAuth from '../hooks/getAuth'
 
 
 function App() {
+  const handleLoginSuccess = async (credentialResponse: CredentialResponse) => {
+    try {
+      const response = await getAuth(credentialResponse);
+      console.log("Server Response:", response);
+    } catch (error) {
+      console.error("Registration failed:", error);
+    }
+  };
 
   return (
-    <GoogleOAuthProvider clientId={"YOUR_CLIENT_ID"}>
-      <GoogleLogin
-        onSuccess={async (credentialResponse: CredentialResponse) => {
-          try {
-            const response = await getAuth(credentialResponse); 
-            console.log("Server Response:", response);
-          } catch (error) {
-            console.error("Registration failed:", error);
-          }
+    <GoogleOAuthProvider clientId={"899796014325-caos2u0vn8g0qrij96kcpvdi4hifq18d.apps.googleusercontent.com"}>
+     <GoogleLogin
+        onSuccess={handleLoginSuccess}
+        onError={() => {
+          console.log('Login Failed');
         }}
-        onError={() => console.log('Login Failed')}
       />
     </GoogleOAuthProvider>
   );
