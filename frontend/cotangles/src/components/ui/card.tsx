1be0e00764d5a6e5ee89a280e4f14f4a35643c81
evaluate09 <eve.miles09@gmail.com>
2024-12-10 22:21:30 +1100

    Added pending calendar invites and made calendars clickable

--- frontend/cotangles/src/components/ui/card.tsx
+++ frontend/cotangles/src/components/ui/card.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import { Plus } from "lucide-react";
-import { Button } from "./button";
+
 
 const CardTop = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
@@ -31,19 +30,7 @@ const CardBody = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
 );
 CardBody.displayName = "CardBody";
 
-const CardBodyCalendar = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        "flex flex-wrap align-center justify-start rounded-[2.5em] bg-transparent text-black w-[70vw] h-[85%] p-5 gap-[5%] overflow-auto",
-        className
-      )}
-      {...props}
-    />
-  )
-);
-CardBodyCalendar.displayName = "CardBodyCalendar";
+
 
 const CardAuth = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
@@ -58,30 +45,6 @@ const CardAuth = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
   )
 );
 CardAuth.displayName = "CardAuth";
-
-type CardCalendarProps = { calendarName: string }
-const CardCalendar = ({ calendarName }: CardCalendarProps) => {
-  return (
-    <div className="flex flex-col align-center items-center justify-evenly rounded-[2.5em] shadow-light border-2 border-border bg-secondary text-black w-[15.5em] h-[70%] p-5">
-        <h1><b>{calendarName}</b></h1>
-        {/* Could add something here like a calendar / person preview , also calendar settings (leave calendar and whatnot) */}
-        <div></div>
-    </div>
-  );
-};
-
-// Todo, link this to back end , below component will have the invites prop
-// type CardCalendarInfoProps = { invites: calendarInvite[] }
-const CardCalendarInfo = () => {
-  return (
-    <div className="flex flex-col align-center items-center justify-between rounded-[2.5em] bg-transparent text-black w-[15.5em] h-[70%]">
-        <Button className="w-[100%] rounded-[2.5em] bg-secondary h-[20%]"><Plus/></Button>
-        <div className="flex flex-col align-center items-center justify-start rounded-[2.5em] shadow-light border-2 border-border bg-secondary text-black w-[100%] h-[70%] p-5">
-          <h1><b>Pending</b></h1>
-        </div>
-    </div>
-  );
-};
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, style, ...props }, ref) => (
@@ -131,10 +94,7 @@ CardFooter.displayName = "CardFooter";
 export {
   CardTop,
   CardBody,
-  CardBodyCalendar,
   CardAuth,
-  CardCalendar,
-  CardCalendarInfo,
   CardHeader,
   CardFooter,
   CardTitle,
--- frontend/cotangles/src/pages/MyCalendars.tsx
+++ frontend/cotangles/src/pages/MyCalendars.tsx
@@ -1,5 +1,6 @@
 import "../App.css";
-import { CardTop, CardHeader, CardCalendar, CardBodyCalendar, CardCalendarInfo } from "@/components/ui/card";
+import { CardTop, CardHeader } from "@/components/ui/card";
+import { CardCalendar, CardBodyCalendar, CardCalendarInfo } from "@/components/ui/calendar-list"
 function MyCalendars() {
   return (
     <>
@@ -12,12 +13,16 @@ function MyCalendars() {
         {/* Body */}
         <CardBodyCalendar>
           <CardCalendarInfo/>
-          <CardCalendar calendarName="aaaaa"/>
-          <CardCalendar calendarName="owo"/>
-          <CardCalendar calendarName="omg"/>
-          <CardCalendar calendarName="mmm"/>
-          <CardCalendar calendarName="aasd"/>
-          <CardCalendar calendarName="fhqwgads"/>
+          {/* 
+          Todo make sure calendarnames are less than 20 characters
+          Probs get function to validate this in the frontend
+          */}
+          <CardCalendar calendarName="Calendar Name" calendarId={1}/>
+          <CardCalendar calendarName="CS Alliance" calendarId={2}/>
+          <CardCalendar calendarName="COMM1140 Team" calendarId={3}/>
+          <CardCalendar calendarName="Team 1: Death" calendarId={4}/>
+          <CardCalendar calendarName="MATH1081 Study Group" calendarId={5}/>
+          <CardCalendar calendarName="wow" calendarId={6}/>
         </CardBodyCalendar>
     </>
   );
