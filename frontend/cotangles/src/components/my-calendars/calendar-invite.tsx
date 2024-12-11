31f137036b6191dd2cb0ddfee2095d82ca96c906
evaluate09 <eve.miles09@gmail.com>
2024-12-11 19:09:47 +1100

    Edited style of the scrollbar

--- frontend/cotangles/src/components/my-calendars/calendar-invite.tsx
+++ frontend/cotangles/src/components/my-calendars/calendar-invite.tsx
@@ -10,15 +10,17 @@ export const CardCalendarInvite = () => {
     return (
       <div className="flex flex-col content-center items-center justify-between rounded-[2.5em] bg-transparent text-black w-[30%] h-[70%]">
         <CalendarCreateForm/>
-          <div className="flex flex-col items-center justify-start rounded-[2.5em] shadow-light border-2 border-border bg-secondary text-black w-full h-[70%] overflow-y-auto">
-            <h1 className="break-words w-full text-md py-2">Pending</h1>
-            <CalendarInvite calendarId={1} calendarName="This is20 characters"/>
-            <CalendarInvite calendarId={2} calendarName="omg"/>
-            <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
-            <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
-            <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
-            <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
-            <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
+          <div className="flex flex-col items-center justify-start rounded-[2.5em] shadow-light border-2 border-border bg-secondary text-black w-full h-[70%]">
+            <h2 className="w-full text-md py-2 break-words">Pending</h2>
+            <div className="overflow-y-auto h-[90%] w-[80%]">
+              <CalendarInvite calendarId={1} calendarName="This is20 characters"/>
+              <CalendarInvite calendarId={2} calendarName="omg"/>
+              <CalendarInvite calendarId={3} calendarName="aaaa"/>
+              <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
+              <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
+              <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
+              <CalendarInvite calendarId={3} calendarName="Gigachad meetups"/>
+            </div>
           </div>
       </div>
     );
@@ -33,10 +35,10 @@ const CalendarInvite = ({ calendarName, calendarId }: CardCalendarProps) => {
     // Removes the invite when you deny
     console.log(calendarId)
     return(
-        <div className="flex justify-center content-center items-center w-[90%] p-0 pb-1">
-            <span className="flex flex-grow h-[80%] min-w-[60%] text-[0.7em] bg-primary text-center justify-center items-center border-2 rounded-lg px-1"><b>{calendarName}</b></span>
-            <Button className="scale-75 w-[20%] rounded-xl bg-affirmative h-full"><Check/></Button>
-            <Button className="scale-75 w-[20%] rounded-xl bg-destructive h-full"><X/></Button>
+        <div className="flex content-center items-center w-[95%] p-0 pb-1">
+            <span className="min-w-[50%] min-h-8 flex flex-grow h-full text-[0.7em] bg-primary text-center justify-center items-center border-2 rounded-lg px-1"><b>{calendarName}</b></span>
+            <Button className="scale-75 flex-grow-0 w-[20%] rounded-xl bg-affirmative h-full"><Check/></Button>
+            <Button className="scale-75 flex-grow-0 w-[20%] rounded-xl bg-destructive h-full"><X/></Button>
         </div>
     )
 }
